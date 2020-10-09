0ac6fcd731df6f1a12a6eb0936aad1f3b6c576b0
hantrungkien <hantrungkien98@gmail.com>
2020-10-09 17:17:43 +0700

    add code

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, LogBox } from 'react-native';
+import Login from './components/Login';
+import CreatAcount from './components/CreatAcount';
+import Header from './components/Header';
+import Friend from './components/Friend';
+import AllFriends from './components/AllFriends';
+import SuggestionFriend from './components/SuggestionFriend';
+import Video from './components/Video';
+import SearchVideo from './components/SearchVideo';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AllFriends></AllFriends>
+      
+ 
     </View>
   );
 }
@@ -14,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    
+   // backgroundColor: "#f4fbd2",
+    marginTop: 20,
+    
+    
+    
   },
+  
+  
 });
+
